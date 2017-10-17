f568e1a5924a464395f6acfb80b6b600194dbb1f
Michal Karm Babacek <karm@fedoraproject.org>
2017-10-17 08:17:43 -0700

    Added libexpat to Git - fixing patch

--- windows/libexpat/build.bat
+++ windows/libexpat/build.bat
@@ -4,8 +4,6 @@ REM This script builds libexpat
 mkdir %WORKSPACE%\target\64
 mkdir %WORKSPACE%\expat\build-64
 
-patch.exe --verbose -p1 CMakeLists.txt -i ci-scripts\windows\libexpat\libexpat-win-CMakeLists.patch
-
 REM Build environment
 set "PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build;C:\Program Files\Cppcheck;%PATH%"
 call vcvars64
@@ -14,6 +12,7 @@ cd %WORKSPACE%\expat\build-64
 
 SET WORKSPACEPOSSIX=%WORKSPACE:\=/%
 
+patch.exe --verbose -p1 %WORKSPACE%\CMakeLists.txt -i %WORKSPACE%\ci-scripts\windows\libexpat\libexpat-win-CMakeLists.patch
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="/O2 /Wall /Zi" -DCMAKE_CXX_FLAGS_RELEASE="/O2 /Wall /Zi" ^
 -DBUILD_tests=OFF -DBUILD_shared=ON -DCMAKE_INSTALL_PREFIX=%WORKSPACEPOSSIX%/target/64/ ..
 
