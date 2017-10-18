dc6394d86888604af981150182a1d282204b625d
Michal Karm Babacek <karm@fedoraproject.org>
2017-10-18 10:24:22 -0700

    APR Util needs Expat; this time via CMake find package plugin

--- windows/apr-util/build.bat
+++ windows/apr-util/build.bat
@@ -3,6 +3,8 @@ REM This script builds apr-util
 REM We rely on label being the same. It is probably for the best, gonna keep the same MSVC...
 unzip label=%label%\apr*.zip -d .\apr
 IF NOT %ERRORLEVEL% == 0 ( exit 1 )
+unzip label=%label%\libexpat*.zip -d .\libexpat
+IF NOT %ERRORLEVEL% == 0 ( exit 1 )
 unzip arch=64,label=%label%\OpenSSL*.zip -d .\openssl
 IF NOT %ERRORLEVEL% == 0 ( exit 1 )
 
@@ -19,6 +21,7 @@ REM Note that some attributes cannot handle backslashes...
 SET WORKSPACEPOSSIX=%WORKSPACE:\=/%
 
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="/MD /O2 /Ob2 /Wall /Zi" ^
+-DEXPAT_INCLUDE_DIRS=%WORKSPACEPOSSIX%/libexpat/include/ -DEXPAT_LIBRARYS=%WORKSPACEPOSSIX%/libexpat/lib/expat.lib ^
 -DAPR_INCLUDE_DIR=%WORKSPACEPOSSIX%/apr/include/ ^
 -DAPR_LIBRARIES=%WORKSPACEPOSSIX%/apr/lib/libapr-1.lib;%WORKSPACEPOSSIX%/apr/lib/libaprapp-1.lib ^
 -DOPENSSL_ROOT_DIR=%WORKSPACEPOSSIX%/openssl/ -DAPU_HAVE_CRYPTO=ON -DAPU_HAVE_ODBC=ON ^
